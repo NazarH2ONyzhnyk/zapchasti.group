4a1fe2a9ba09db4dfcea28da5464441942a35755
NazarH2ONyzhnyk <nazarh2onyzhnyk@gmail.com>
2022-07-04 10:47:46 +0300

    project

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        yellow: {
+          300: "#FEBF2D",
+          400: "#FDB819"
+        },
+        black: {
+          200: "#1D1D1D",
+          300: "#141414",
+          900: "#000000"
+        },
+        gray: {
+          100: "#F5F5F5",
+          200: "#E2E2E2",
+          300: "#CBCBCB",
+          400: "#9B9B9B",
+        }
+      },
+    },
+  },
+  plugins: [],
+}
