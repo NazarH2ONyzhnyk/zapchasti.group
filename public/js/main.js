6ee476774feb5c86d05693e79daa4a4b108131b2
NazarH2ONyzhnyk <nazarh2onyzhnyk@gmail.com>
2022-08-09 04:34:52 +0300

    додаткові файли

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,33 @@
-let count = document.getElementById("count")
-function desc() {
-    if(count.value <=1) {
-        count.value =1
-    } else {
-        count.value --
-    }
+var signin = document.getElementById('signin')
+var signup = document.getElementById('signup')
+var loginForm = document.getElementById('loginForm')
+var registerForm = document.getElementById('registerForm')
+
+
+// if true = login // else if false = sign up
+var signStatus = true
+
+function signIn() {
+    if(signStatus == false) {
+
+        signup.classList.replace('login__switch__active','login__switch__disabled')
+        signin.classList.replace('login__switch__disabled','login__switch__active')
+        loginForm.classList.replace('hidden','block')
+        registerForm.classList.replace('block', 'hidden')
+        
+        
+        signStatus = true
+    } 
 }
-function add() {
-    count.value ++
-    console.log('adding')
+
+function signUp() {
+    if(signStatus == true) {
+        
+        signup.classList.replace('login__switch__disabled','login__switch__active')
+        signin.classList.replace('login__switch__active','login__switch__disabled')
+        registerForm.classList.replace('hidden', 'block')
+        loginForm.classList.replace('block','hidden')
+
+        signStatus = false
+    }
 }
